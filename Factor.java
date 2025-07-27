46d1e06a1359f897258fc46076373e392a4e7238
anushka20251 <anushkabhosale2121@gmail.com>
2025-07-27 17:45:54 +0530

    Add files via upload

---
+++ Factor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factor {
+    public static void main(String[] args){
+       
+        isfactor();
+    }
+ public static boolean isfactor(){
+    Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a number");
+     
+    int number=sc.nextInt();
+    for(int i=1;i<=number;i++)
+    {
+        if(number%i==0)
+    {
+         System.out.println(i);
+    }
+    }
+    return true;
+    
+    
+ }
+}
